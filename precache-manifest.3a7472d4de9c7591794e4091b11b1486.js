01170f928d11e3662ff5ea31b22f4e0298cee645
Sachin Mali <smali@adobe.com>
2022-07-25 18:30:36 -0400

    Updates

---
+++ precache-manifest.3a7472d4de9c7591794e4091b11b1486.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa269ff7d9ecb461f645a8c27c32d84",
+    "url": "/aem-headless-basic-tutorial/index.html"
+  },
+  {
+    "revision": "78c0f0a1e5bc6961a17a",
+    "url": "/aem-headless-basic-tutorial/static/css/main.c08dfc50.chunk.css"
+  },
+  {
+    "revision": "f6e0ab9bc88f428831e6",
+    "url": "/aem-headless-basic-tutorial/static/js/2.3955c4bb.chunk.js"
+  },
+  {
+    "revision": "7d6cd994641ab36f6d0560205f58ca87",
+    "url": "/aem-headless-basic-tutorial/static/js/2.3955c4bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c0f0a1e5bc6961a17a",
+    "url": "/aem-headless-basic-tutorial/static/js/main.472be749.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/aem-headless-basic-tutorial/static/js/main.472be749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "987e669013c937485301",
+    "url": "/aem-headless-basic-tutorial/static/js/runtime-main.6998cfd1.js"
+  },
+  {
+    "revision": "200bc7ed9cfe0f3bde2835852b50271e",
+    "url": "/aem-headless-basic-tutorial/static/media/icon-loading.200bc7ed.svg"
+  },
+  {
+    "revision": "180f5e5f65a031a0bed8fca8bc79fdfb",
+    "url": "/aem-headless-basic-tutorial/static/media/wknd-icon.180f5e5f.svg"
+  }
+]);
